bde5852dea60c7e8ff3b11dacec108c652effd9f
deepakpathik <deepakpathik2005@gmail.com>
2026-01-21 20:02:17 +0530

    feat: add client-side socket service

---
+++ src/services/socketService.ts
@@ -0,0 +1,50 @@
+import { io, Socket } from 'socket.io-client';
+
+const SOCKET_URL = 'http://localhost:5000';
+
+class SocketService {
+    private socket: Socket | null = null;
+
+    public connect(): Socket {
+        if (this.socket?.connected) {
+            return this.socket;
+        }
+
+        this.socket = io(SOCKET_URL, {
+            reconnectionAttempts: 5,
+            reconnectionDelay: 1000,
+            autoConnect: true,
+        });
+
+        this.socket.on('connect', () => {
+            console.log('Socket connected:', this.socket?.id);
+        });
+
+        this.socket.on('disconnect', () => {
+            console.log('Socket disconnected');
+        });
+
+        this.socket.on('connect_error', (err) => {
+            console.error('Socket connection error:', err);
+        });
+
+        return this.socket;
+    }
+
+    public disconnect() {
+        if (this.socket) {
+            this.socket.disconnect();
+            this.socket = null;
+        }
+    }
+
+    public getSocket(): Socket | null {
+        return this.socket;
+    }
+
+    public isConnected(): boolean {
+        return this.socket?.connected || false;
+    }
+}
+
+export const socketService = new SocketService();
